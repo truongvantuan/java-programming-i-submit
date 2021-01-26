97fde7f3e30a0a92eff01661d48f518e423c8ca9
truongvantuan <truongvantuan@outlook.com.vn>
2021-01-26 09:17:51 +0700

    submit part01_07

--- part01-Part01_07.MessageThreeTimes/src/main/java/MessageThreeTimes.java
+++ part01-Part01_07.MessageThreeTimes/src/main/java/MessageThreeTimes.java
@@ -5,9 +5,10 @@ public class MessageThreeTimes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Write a message:");
-        // Write your program here
-
+        System.out.println("Write a message: ");
+        String message = scanner.nextLine();
+        System.out.println(message);
+        System.out.println(message);
+        System.out.println(message);
     }
 }
