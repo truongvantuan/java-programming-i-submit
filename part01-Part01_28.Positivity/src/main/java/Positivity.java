a0bc94b287aaa2893e2bfe12c2d907cb9b2f0d1a
truongvantuan <truongvantuan@outlook.com.vn>
2021-01-28 09:36:30 +0700

    submit part01_28->31

--- part01-Part01_28.Positivity/src/main/java/Positivity.java
+++ part01-Part01_28.Positivity/src/main/java/Positivity.java
@@ -5,7 +5,13 @@ public class Positivity {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        System.out.println("Give a number:");
+        int number = Integer.valueOf(scan.nextLine());
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is not positive.");
+        }
         // Write your program here
     }
 }
--- part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
+++ part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
@@ -4,7 +4,19 @@ import java.util.Scanner;
 public class LargerThanOrEqualTo {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int firstNumber = Integer.valueOf(scanner.nextLine());
 
+        System.out.println("Give the second number:");
+        int secondNumber = Integer.valueOf(scanner.nextLine());
+
+        if (firstNumber == secondNumber) {
+            System.out.println("The numbers are equal!");
+        } else if (firstNumber > secondNumber) {
+            System.out.println("Greater number is: " + firstNumber);
+        } else {
+            System.out.println("Greater number is: " + secondNumber);
+        }
     }
 }
