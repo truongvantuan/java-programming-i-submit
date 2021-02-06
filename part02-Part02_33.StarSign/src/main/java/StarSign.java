c9370670e9926bd3bec5ccff921117b8c9cee74d
truongvantuan <truongvantuan@outlook.com.vn>
2021-02-06 10:07:00 +0700

    submit part02_23->34 | finish part_02

--- part02-Part02_33.StarSign/src/main/java/StarSign.java
+++ part02-Part02_33.StarSign/src/main/java/StarSign.java
@@ -9,6 +9,7 @@ public class StarSign {
         //in the main to make sure they print the correct shapes!
         
         printStars(3);
+        printStars(5);
         System.out.println("\n---");  // printing --- between the shapes
         printSquare(4);
         System.out.println("\n---");
@@ -20,17 +21,32 @@ public class StarSign {
 
     public static void printStars(int number) {
         // first part of the exercise
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
     }
 
     public static void printSquare(int size) {
         // second part of the exercise
+        int row = 1;
+        while (row <= size) {
+            printStars(size);
+            row++;
+        }
     }
 
     public static void printRectangle(int width, int height) {
         // third part of the exercise
+        for (int i = 0; i < height; i++) {
+            printStars(width);
+        }
     }
 
     public static void printTriangle(int size) {
         // fourth part of the exercise
+        for (int i = 1; i <= size; i++) {
+            printStars(i);
+        }
     }
 }
--- part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -2,19 +2,39 @@
 public class AdvancedAstrology {
 
     public static void printStars(int number) {
-        // part 1 of the exercise
+        // first part of the exercise
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
     }
 
     public static void printSpaces(int number) {
         // part 1 of the exercise
+        for (int i = 0; i < number; i++) {
+            System.out.print(" ");
+        }
     }
 
     public static void printTriangle(int size) {
         // part 2 of the exercise
+        for (int i = 0; i < size; i++) {
+            printSpaces(size - i - 1);
+            printStars(i + 1);
+        }
     }
 
     public static void christmasTree(int height) {
         // part 3 of the exercise
+        for (int i = 1; i <= height; i++) {
+            printSpaces(height - i);
+            printStars(2 * i - 1);
+        }
+
+        printSpaces(2 * height / 2 - 2);
+        printStars(3);
+        printSpaces(2 * height / 2 - 2);
+        printStars(3);
     }
 
     public static void main(String[] args) {
