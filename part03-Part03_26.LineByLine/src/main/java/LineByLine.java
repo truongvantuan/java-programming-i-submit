d12455ad62cf5a1d0e41f1ff4c2f91a1052aa48a
truongvantuan <truongvantuan@outlook.com.vn>
2021-02-11 22:09:13 +0700

    submit part03_26->27

--- part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -6,6 +6,17 @@ public class LineByLine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        while (true) {
+            String str = scanner.nextLine();
+            
+            if (str.equals("")) {
+                break;
+            } else {
+                String[] words = str.split(" ");
+                for (String item : words) {
+                    System.out.println(item);
+                }
+            }
+        }
     }
 }
--- part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -6,6 +6,18 @@ public class AVClub {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        while (true) {
+            String str = scanner.nextLine();
+            if (str.equals("")) {
+                break;
+            } else {
+                String[] words = str.split(" ");
+                for (String word : words) {
+                    if (word.contains("av")) {
+                        System.out.println(word);
+                    }
+                }
+            }
+        }
     }
 }
