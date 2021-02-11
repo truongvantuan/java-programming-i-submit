b81da4b81020dc0d6a0809c209816b7ba45201b0
truongvantuan <truongvantuan@outlook.com.vn>
2021-02-12 00:36:08 +0700

    submit part03_28->29

--- part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -6,6 +6,15 @@ public class FirstWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String str = scanner.nextLine();
+            if (str.equals("")) {
+                break;
+            } else {
+                String[] words = str.split(" ");
+                System.out.println(words[0]);
+            }
+        }
 
     }
 }
--- part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -6,6 +6,15 @@ public class LastWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String str = scanner.nextLine();
+            if (str.equals("")) {
+                break;
+            } else {
+                String[] words = str.split(" ");
+                System.out.println(words[words.length - 1]);
+            }
+        }
 
     }
 }
