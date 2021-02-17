6745a5568b5dcf8b1732ac8759822b5da6f0ab7e
truongvantuan <truongvantuan@outlook.com.vn>
2021-02-17 21:18:17 +0700

    submit part04_14->15

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -12,5 +12,33 @@ public class MainProgram {
         // correct order in the main program. First the object that tracks the total
         // sum, secondly the object that tracks the sum of even numbers, 
         // and lastly the one that tracks the sum of odd numbers!
+        Statistics statistics = new Statistics();
+        Statistics statisticsEven = new Statistics();
+        Statistics statisticsOdd = new Statistics();
+
+//        statistics.addNumber(3);
+//        statistics.addNumber(5);
+//        statistics.addNumber(1);
+//        statistics.addNumber(2);
+//        System.out.println("Count: " + statistics.getCount());
+//        System.out.println("Sum: " + statistics.sum());
+//        System.out.println("Average: " + statistics.average());
+
+//        statistics.addNumber(-statistics.sum());
+        System.out.println("Enter numbers:");
+        while (true) {
+            int input = scanner.nextInt();
+            if (input == -1) {
+                break;
+            } else if (input % 2 == 0) {
+                statisticsEven.addNumber(input);
+            } else {
+                statisticsOdd.addNumber(input);
+            }
+            statistics.addNumber(input);
+        }
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Sum of even numbers: " + statisticsEven.sum());
+        System.out.println("Sum of odd numbers: " + statisticsOdd.sum());
     }
 }
--- part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,31 @@
 
 public class Statistics {
+
+    private int count;
+    private int sum;
+
+    public Statistics() {
+        // initialize the variable numberCount here
+    }
+
+    public void addNumber(int number) {
+        this.count++;
+        this.sum += number;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    public int sum() {
+        return this.sum;
+    }
+
+    public double average() {
+        double average = 0;
+        if (this.count != 0) {
+            average = (double) this.sum / this.count;
+        }
+        return average;
+    }
 }
