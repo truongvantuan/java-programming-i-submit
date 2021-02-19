77bb35889cc581de2c4e7debb7925ef4362f0c39
truongvantuan <truongvantuan@outlook.com.vn>
2021-02-19 09:28:36 +0700

    submit part04_28->29

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -1,5 +1,6 @@
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NumbersFromAFile {
@@ -13,6 +14,28 @@ public class NumbersFromAFile {
         int lowerBound = Integer.valueOf(scanner.nextLine());
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
+        int count = 0;
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        try (Scanner fileInput = new Scanner(Paths.get(file))) {
+
+            while (fileInput.hasNextLine()) {
+
+                String line = fileInput.nextLine();
+                list.add(Integer.parseInt(line));
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            if (lowerBound <= list.get(i) && list.get(i) <= upperBound) {
+                count++;
+            }
+        }
+
+        System.out.println("Numbers: " + count);
 
     }
 
--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -7,5 +7,19 @@ public class RecordsFromAFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Name of the file: ");
+        String file = scanner.nextLine();
+
+        try (Scanner fileInput = new Scanner(Paths.get(file))) {
+
+            while (fileInput.hasNextLine()) {
+                String line = fileInput.nextLine();
+                String[] part = line.split(",");
+                
+                System.out.println(part[0] + ", age: " + part[1] + " years");
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
