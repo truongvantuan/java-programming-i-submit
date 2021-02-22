4a92d0beab56c64923eb9147e4986464573490e4
truongvantuan <truongvantuan@outlook.com.vn>
2021-02-22 11:16:33 +0700

    submit part05_02

--- part05-Part05_02.Book/src/main/java/Program.java
+++ part05-Part05_02.Book/src/main/java/Program.java
@@ -6,9 +6,8 @@ public class Program {
         // free to experiment with your Book class.
         // An example that you can use for testing:
 
-
-//        Book b = new Book("J. K. Rowling", "Harry Potter and the Sorcerer's Stone", 223);
-//        System.out.println(b);
+        Book b = new Book("J. K. Rowling", "Harry Potter and the Sorcerer's Stone", 223);
+        System.out.println(b);
 
     }
 }
