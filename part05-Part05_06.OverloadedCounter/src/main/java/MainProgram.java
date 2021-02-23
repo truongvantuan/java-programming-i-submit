3b70270b74c8e6bc52a4da49f3cc7803fea69215
truongvantuan <truongvantuan@outlook.com.vn>
2021-02-23 10:00:34 +0700

    submit part05_06

--- part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
+++ part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
@@ -3,5 +3,13 @@ public class MainProgram {
 
     public static void main(String[] args) {
         // Test your counter here
+        Counter counter = new Counter();
+        System.out.println(counter.value());
+
+        counter.increase();
+        System.out.println(counter.value());
+        
+        counter.increase(10);
+        System.out.println(counter.value());
     }
 }
