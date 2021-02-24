cfe04694619a9e9208a015b846d9d4919144c326
truongvantuan <truongvantuan@outlook.com.vn>
2021-02-24 10:56:37 +0700

    submit part05_09 | fetch new exercies

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,20 @@
 
 public class HealthStation {
 
+    private int countWeighing;
 
     public int weigh(Person person) {
         // return the weight of the person passed as the parameter
-        return -1;
+        countWeighing++;
+        return person.getWeight();
+    }
+
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
+
+    public int weighings() {
+        return this.countWeighing;
     }
 
 }
