4e2b02f2e217954876a81a918a71a3a9e2990ce8
truongvantuan <truongvantuan@outlook.com.vn>
2021-03-01 20:59:29 +0700

    submit part07_03

--- part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -1,9 +1,52 @@
 
+import static java.lang.System.arraycopy;
+import java.util.Arrays;
+
 public class MainProgram {
 
     public static void main(String[] args) {
-        // write your test code here
-        int[] array = {3, 1, 5, 99, 3, 12};
+        int[] numbers = {8, 3, 7, 9, 1, 2, 4};
+        MainProgram.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
     }
 
+    public static int smallest(int[] array) {
+        int smallest = array[0];
+
+        for (int i = 0; i < array.length; i++) {
+            smallest = array[i] < smallest ? array[i] : smallest;
+        }
+        return smallest;
+    }
+
+    public static int indexOfSmallest(int[] array) {
+        int index = 0;
+        int value = smallest(array);
+
+        for (int i = 0; i < array.length; i++) {
+            index = value == array[i] ? i : index;
+        }
+        return index;
+    }
+
+    public static int indexOfSmallestFrom(int[] table, int fromIndex) {
+        int[] targetTable = new int[table.length - fromIndex];
+        for (int i = fromIndex; i < table.length; i++) {
+            targetTable[i - fromIndex] = table[i];
+        }
+        return indexOfSmallest(targetTable) + fromIndex;
+    }
+
+    public static void swap(int[] array, int index1, int index2) {
+        int save = array[index2];
+        array[index2] = array[index1];
+        array[index1] = save;
+    }
+
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int index = indexOfSmallestFrom(array, i);
+            swap(array, index, i);
+        }
+    }
 }
