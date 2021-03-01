62952a0b6977ee14f17c4fd9d1d34791d0abe78e
truongvantuan <truongvantuan@outlook.com.vn>
2021-03-01 13:57:06 +0700

    submit part06_13

--- part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
+++ part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
@@ -1,4 +1,43 @@
 
+import java.util.ArrayList;
+
 public class ExerciseManagement {
 
+    private ArrayList<Exercise> exercises;
+//    private ArrayList<String> completedList;
+
+    public ExerciseManagement() {
+        this.exercises = new ArrayList<>();
+//        this.completedList = new ArrayList<>();
+    }
+
+    public ArrayList<String> exerciseList() {
+        ArrayList<String> list = new ArrayList<>();
+        for (Exercise ex : exercises) {
+            list.add(ex.getName());
+        }
+
+        return list;
+    }
+
+    public void add(String exercise) {
+        this.exercises.add(new Exercise(exercise));
+    }
+
+    public void markAsCompleted(String exercise) {
+        for (Exercise ex : exercises) {
+            if (ex.getName().equals(exercise)) {
+                ex.setCompleted(true);
+            }
+        }
+    }
+
+    public boolean isCompleted(String exercise) {
+        for (Exercise ex : exercises) {
+            if (ex.getName().equals(exercise)) {
+                return ex.isCompleted();
+            }
+        }
+        return false;
+    }
 }
