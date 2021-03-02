a1802ab50d68f35bffd0ffd11f99a8733e723269
truongvantuan <truongvantuan@outlook.com.vn>
2021-03-02 09:42:08 +0700

    submit part07_05

--- part07-Part07_05.Searching/src/main/java/Searching.java
+++ part07-Part07_05.Searching/src/main/java/Searching.java
@@ -8,6 +8,7 @@ public class Searching {
         // The program below is meant for testing the search algorithms you'll write
         Scanner scanner = new Scanner(System.in);
         ArrayList<Book> books = new ArrayList<>();
+        
         System.out.println("How many books to create?");
         int numberOfBooks = Integer.valueOf(scanner.nextLine());
         for (int i = 0; i < numberOfBooks; i++) {
@@ -44,11 +45,35 @@ public class Searching {
     }
 
     public static int linearSearch(ArrayList<Book> books, int searchedId) {
-        return -1;
+        int index = -1;
+
+        for (int i = 0; i < books.size(); i++) {
+            index = books.get(i).getId() == searchedId ? i : index;
+        }
+
+        return index;
     }
 
     public static int binarySearch(ArrayList<Book> books, long searchedId) {
-        return -1;
+        int begin = 0;
+        int end = books.size() - 1;
+        int index = -1;
+        
+        while (begin <= end) {
+            int middle = (begin + end) / 2;
+            long middleId = books.get(middle).getId();
+            
+            if (middleId == searchedId) {
+                index = middle;
+                break;
+            }
+
+            if (middleId < searchedId) {
+                begin = middle + 1;
+            } else {
+                end = middle - 1;
+            }
+        }
+        return index;
     }
 }
-
