3a084760d2bf81ad25791cca75bf727b27d16425
truongvantuan <truongvantuan@outlook.com.vn>
2021-03-02 08:55:43 +0700

    submit part07_04

--- part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
+++ part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
@@ -1,10 +1,38 @@
 
+//import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         // insert test code here
         int[] array = {3, 1, 5, 99, 3, 12};
+        String[] strings = {"a","b","u","d","n"};
+        sort(array);
+        sort(strings);
+        
+        System.out.println(Arrays.toString(strings));
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void sort(int[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sort(String[] array) {
+        List<String> list = Arrays.asList(array);
+        Collections.sort(list);
+    }
+
+    public static void sortIntegers(ArrayList<Integer> integers) {
+        Collections.sort(integers);
+    }
+
+    public static void sortStrings(ArrayList<String> strings) {
+        Collections.sort(strings);
     }
 
 }
